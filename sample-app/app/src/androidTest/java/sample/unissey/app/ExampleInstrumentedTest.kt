c5bf3100cb37e996c853a5efe011889d667617b7
gtindo <gtyletindo@gmail.com>
2022-04-04 13:44:50 +0200

    rename deepsense to unissey, add scrollview to video player

--- sample-app/app/src/androidTest/java/sample/unissey/app/ExampleInstrumentedTest.kt
+++ sample-app/app/src/androidTest/java/sample/unissey/app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("co.deepsense.android_sample_app", appContext.packageName)
+        assertEquals("sample.unissey.app", appContext.packageName)
     }
 }
